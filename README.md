d2bb6d466771aa4d26c14a9fba9aa3fd164c3a70
taijiji <tuty0630@gmail.com>
2016-11-26 18:52:05 +0900

    Add how to run, output on README add show device hostname on set_firefly1_change_hostname.py

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # sample_pyez
 Sample code for using Juniper PyEZ
+
+
+## sample code 1 : change hostname
+How to run sample code 1
+
+```
+python set_firefly1_change_hostname.py
+```
+
+output
+
+```                                                                                                                                                   (git)-[master]
+##### Operation : Start #######
+Connecting to device :  OK
+Hostname :  firefly1
+Load config :  OK
+Target config : ./configs/change_hostname.conf
+##############################
+system {
+    host-name firefly1_changed_by_PyEZ;
+}
+##############################
+Diff :
+##############################
+
+[edit system]
+-  host-name firefly1;
++  host-name firefly1_changed_by_PyEZ;
+
+##############################
+Commit Check :  OK
+Do you commit? y/n
+y
+Commit candidate config : OK
+Hostname :  firefly1_changed_by_PyEZ
+```
--- set_firefly1_change_hostname.py
+++ set_firefly1_change_hostname.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# PyEZ
 from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
 
+# color font
 import colorama
 from colorama import Fore, Back, Style
 
@@ -13,33 +15,28 @@ print "##### Operation : Start #######"
 
 print "Connecting to device : ",
 dev1 = Device(
-            host='192.168.34.16',
-            user='user1',
+            host="192.168.34.16",
+            user="user1",
             port=22, # NETCONF port (defaults to 830)
-            password='password1'
+            password="password1"
         )   
 dev1.open()
-print Fore.GREEN + 'OK'
-
+print Fore.GREEN + "OK"
 
 print "Hostname : ",
-print dev1.facts['hostname']
-
+print dev1.facts["hostname"]
 
 dev1.bind(cu=Config)
 
-
 print "Load config : ",
-
 dev1.cu.lock()
 conf_filename = "./configs/change_hostname.conf"
 dev1.cu.load(path=conf_filename, format="text", merge=True)
-
-print Fore.GREEN + 'OK' 
+print Fore.GREEN + "OK" 
 
 print "Target config : %s" % (conf_filename)
 print "#"*30
-with open(conf_filename, 'r') as conf:
+with open(conf_filename, "r") as conf:
     print conf.read()
 print "#"*30
 
@@ -50,13 +47,13 @@ print "#"*30
 
 print "Commit Check : ",
 if dev1.cu.commit_check() :
-    print Fore.GREEN + 'OK'
+    print Fore.GREEN + "OK"
 else:
-    print Fore.RED + 'Error'
+    print Fore.RED + "Error"
 
 print Fore.YELLOW + "Do you commit? y/n"
 choice = raw_input().lower()
-if choice == 'y':
+if choice == "y":
     dev1.cu.commit()
     print "Commit candidate config : " + Fore.GREEN + "OK"
 else:
@@ -64,10 +61,13 @@ else:
     print "Rollback : " + Fore.GREEN + "OK"
 
 dev1.cu.unlock()
-
-print "Hostname : ",
-print dev1.facts['hostname']
-
 dev1.close()
+
+dev1.open()
+print "Hostname : ",
+print dev1.facts["hostname"]
+dev1.close()
+
+
 
 "##### Operation : End #####"
